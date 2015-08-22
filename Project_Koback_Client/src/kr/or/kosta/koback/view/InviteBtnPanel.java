84e5d6c792b3d29ce7b711936725e58ff2dfc6d8
lovesky411 <nomad411@naver.com>
2015-08-22 13:48:48 +0900

    합치기
    합쳐

--- Project_Koback_Client/src/kr/or/kosta/koback/view/InviteBtnPanel.java
+++ Project_Koback_Client/src/kr/or/kosta/koback/view/InviteBtnPanel.java
@@ -5,15 +5,12 @@ import java.awt.Component;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
-<<<<<<< HEAD
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-=======
 import java.util.ArrayList;
 import java.util.List;
->>>>>>> origin/master
 
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
@@ -27,7 +24,7 @@ public class InviteBtnPanel extends JFrame {
 
 	JPanel inviteP; // 초대버튼을 누르면 방장에게 보여지는 패널
 	JLabel choiceL; // 상단 "초대할 사람을 선택하세요"
-	JList<String> userJList; // 대기실 유저 리스트
+	JList<String>userJList ; // 대기실 유저 리스트
 	JButton inviteB, cancelB; // 초대버튼과 취소버튼
 	DefaultListModel<String> model;
 
@@ -52,20 +49,16 @@ public class InviteBtnPanel extends JFrame {
 		cons = new GridBagConstraints();
 
 		setComponents();
-<<<<<<< HEAD
 		eventRegist();
 		
 		
-=======
->>>>>>> origin/master
 	}
 
 	public void setComponents() {
 		setLayout(grid);
 		cons.fill = GridBagConstraints.BOTH;
-<<<<<<< HEAD
 	    cons.insets = new Insets(2,1,2,1);
-	    JScrollPane sp = new JScrollPane(userList);
+	    JScrollPane sp = new JScrollPane(userJList);
 		
 	    add(choiceL,		1, 0, 5, 1, 0.0, 0.0);
 	    add(new JLabel(""), 1, 2, 5, 1, 0.0, 0.0);
@@ -79,28 +72,6 @@ public class InviteBtnPanel extends JFrame {
 	    
 	}
 	
-	 public void inviteUserList(){
-		 
-//	      String[] list = {"가승호(eifwljef)", "용용", "비닝이"};
-//	      for (String string : list) {
-//	         model.addElement(string);
-//	      }
-	   }
-	 
-	 
-	
-	 /*gridBagLayout에 컴포넌트 추가 메소드*/
-	   private void add(Component component, int gridx, int gridy, int gridwidth, int gridheight, double weightx, double weighty){
-	      cons.gridx = gridx;
-	      cons.gridy = gridy;
-	      cons.gridwidth  = gridwidth;
-	      cons.gridheight  = gridheight;
-	      cons.weightx = weightx;
-	      cons.weighty = weighty;
-	      grid.setConstraints(component, cons);
-	      add(component);
-	   }
-
 		public void exit() {
 			setVisible(false);
 			dispose();
@@ -128,25 +99,6 @@ public class InviteBtnPanel extends JFrame {
 		   
 		   
 	   }
-	   
-	   
-	
-	
-	
-    
-=======
-		cons.insets = new Insets(2, 1, 2, 1);
-		JScrollPane sp = new JScrollPane(userJList);
-
-		add(choiceL, 1, 0, 5, 1, 0.0, 0.0);
-		add(new JLabel(""), 1, 2, 5, 1, 0.0, 0.0);
-		add(new JLabel(""), 0, 3, 1, 1, 0.0, 0.0);
-		add(sp, 1, 3, 10, 5, 0.0, 0.0);
-		add(new JLabel(""), 9, 2, 5, 1, 0.0, 0.0);
-		add(inviteB, 1, 10, 1, 1, 0.05, 0.0);
-		add(new JLabel(""), 3, 10, 2, 1, 0.0, 0.0);
-		add(cancelB, 5, 10, 1, 1, 0.05, 0.0);
-	}
 
 	public void chatUserList(List<String> userList) {
 
@@ -172,5 +124,4 @@ public class InviteBtnPanel extends JFrame {
 		add(component);
 	}
 
->>>>>>> origin/master
 }
